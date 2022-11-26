923a1599406066b53d67213f8178e5d8e5f1deae
unknown <minhnghiale1590@gmail.com>
2022-11-26 16:42:53 +0700

    Added mo ta

--- btl-web/admin/newsManagement.php
+++ btl-web/admin/newsManagement.php
@@ -17,6 +17,11 @@
   <link rel="stylesheet" href="admin-styles/styles.css">
   <title>Quản lý tin tức</title>
 </head>
+<style>
+  .bodyContent{
+    max-width:200px;
+  }
+</style>
 <body>
 
   <!-- nav bar --> 
@@ -26,8 +31,10 @@
   </div>
   <!-- end nav bar --> 
     <form method="post" action="newsManagement.php">
+      <a href="index.php"><input type="button" class="p-3 mb-2 bg-gray bg-gradient text-black" value="Trở lại"></a>
       <a href="newNews.php"><input type="button" class="p-3 mb-2 bg-primary bg-gradient text-white" value="Thêm tin tức"></a>
     </form>
+    <div class="container"></div>
     <?php
       include('../config/config.php');
       echo "<table class='table table-bordered'>
@@ -35,7 +42,9 @@
                   <th>ID</th>
                   <th>Tên bài viết</th>
                   <th>Ngày giờ đăng bài</th>
+                  <th>Tiêu đề bài viết</th>
                   <th>Nội dung bài viết</th>
+                  <th>Phím tắt</th>
               </tr>
               <tbody>";
       $sql = "SELECT * from news";
@@ -47,20 +56,22 @@
           $datetime = $row['datetime'];
           $datetime = substr($datetime, 0, 10) . "T" . substr($datetime, 11);
           $content = $row['content'];
+          $contentBody=$row['contentBody'];
           echo "<tr>
                   <td>" . $id . "</td>
                   <td>" . $name . "</td>
                   <td>" . $datetime . "</td>
                   <td>" . $content . "</td>
+                  <td class=\"bodyContent\">" . $contentBody . "</td>
                   <td>
-                    <a href='editNews.php?id=$id&name=$name&datetime=$datetime&content=$content' class='btn btn-primary m-r-1em' name='edit'>Sửa</a>
+                    <a href='editNews.php?id=$id' class='btn btn-primary m-r-1em' name='edit'>Sửa</a>
                     <a href='deleteNews.php?id=$id' class='btn btn-danger'>Xóa</a>
-                    <a href='../newsInfo.php?id=$id' class='btn btn-success'>Xem</a>
+                    <a href='../newsInfo.php?news_id=$id' class='btn btn-success'>Xem</a>
                   </td>
                 </tr>";
         }
-        echo "</tbody></table>";
       }
-    ?>
+      ?>
+    </div>
 </body>
 </html>
