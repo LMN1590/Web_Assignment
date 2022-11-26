e790316c9b51745bd0500aab89fb60b7af79c4fc
unknown <minhnghiale1590@gmail.com>
2022-11-26 12:05:02 +0700

    fixed dropdown for account setting

--- btl-web/productsList.php
+++ btl-web/productsList.php
@@ -21,8 +21,8 @@
   <!-- jsdelivr CDN / Sweet Alert2-->
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <!-- Bootstrap CDN-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
   <!-- CSS -->
   <link rel="stylesheet" href="styles/product-style.css">
