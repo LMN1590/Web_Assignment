643d3461c70ea55c11830338c1d72c4b100d4ca8
unknown <minhnghiale1590@gmail.com>
2022-11-26 17:32:06 +0700

    format UI for admin

--- btl-web/admin/memberManagement.php
+++ btl-web/admin/memberManagement.php
@@ -16,6 +16,28 @@
   <!-- CSS -->
   <link rel="stylesheet" href="admin-styles/styles.css">
   <title>Admin Panel</title>
+  <style>
+    .pass{
+      max-width: 25px;
+      word-break: break-all;
+    }
+    .full{
+      min-width: 100px;
+      line-break: normal;
+      word-break: keep-all;
+    }
+    .mail{
+      max-width: 300px;
+      line-break: anywhere;
+    }
+    .addr{
+      max-width: 300px;
+      line-break: normal;
+    }
+    .btnn{
+      min-width: 150px;
+    }
+  </style>
 </head>
 <body>
 
@@ -29,53 +51,56 @@
     <a href="index.php"><input type="button" class="p-3 mb-2 bg-gray bg-gradient text-black" value="Trở lại"></a>
     <a href="newMember.php"><input type="button" class="p-3 mb-2 bg-primary bg-gradient text-white" value="Thêm thành viên"></a>
   </form>
-  <?php
-    include('../config/config.php');
-    echo "<table class='table table-bordered'>
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Mật khẩu</th>
-                <th>Họ & tên</th>
-                <th>Giới tính</th>
-                <th>Sinh nhật</th>
-                <th>Email</th>
-                <th>Số điện thoại</th>
-                <th>Địa chỉ</th>
-                <th>Phím tắt</th>
-            </tr>
-            <tbody>";
-    $sql = "SELECT * from user";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-      while ($row = $result->fetch_assoc()) {
-        $id = $row['id'];
-        $username = $row['username'];
-        $password = $row['password'];
-        $full_name = $row['full_name'];
-        $seggs = $row['sex'];
-        $birthday = $row['birthday'];
-        $email = $row['email'];
-        $phone = $row['phone'];
-        $address = $row['address'];
-        echo "<tr>
-                <td>" . $id . "</td>
-                <td>" . $username . "</td>
-                <td>" . $password . "</td>
-                <td>" . $full_name . "</td>
-                <td>" . $seggs . "</td>
-                <td>" . $birthday . "</td>
-                <td>" . $email . "</td>
-                <td>" . $phone . "</td>
-                <td>" . $address . "</td>
-                <td>
-                  <a href='editMember.php?id=$id' class='btn btn-primary m-r-1em' name='edit'>Sửa</a>
-                  <a href='deleteMember.php?id=$id' class='btn btn-danger'>Xóa</a>
-                </td>
-              </tr>";
+  <div class="container">
+    <?php
+      include('../config/config.php');
+      echo "<table class='table table-bordered'>
+              <tr>
+                  <th>ID</th>
+                  <th>Username</th>
+                  <th>Mật khẩu</th>
+                  <th>Họ & tên</th>
+                  <th>Giới tính</th>
+                  <th>Sinh nhật</th>
+                  <th>Email</th>
+                  <th>Số điện thoại</th>
+                  <th>Địa chỉ</th>
+                  <th>Phím tắt</th>
+              </tr>
+              <tbody>";
+      $sql = "SELECT * from user";
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+          $id = $row['id'];
+          $username = $row['username'];
+          $password = $row['password'];
+          $full_name = $row['full_name'];
+          $seggs = $row['sex'];
+          $birthday = $row['birthday'];
+          $email = $row['email'];
+          $phone = $row['phone'];
+          $address = $row['address'];
+          echo "<tr>
+                  <td class=\"id\">" . $id . "</td>
+                  <td class=\"usr\">" . $username . "</td>
+                  <td class=\"pass\">" . $password . "</td>
+                  <td class=\"full\">" . $full_name . "</td>
+                  <td class=\"xx\">" . $seggs . "</td>
+                  <td class=\"bir\">" . $birthday . "</td>
+                  <td class=\"mail\">" . $email . "</td>
+                  <td class=\"phone\">" . $phone . "</td>
+                  <td class=\"addr\">" . $address . "</td>
+                  <td class=\"btnn\">
+                    <a href='editMember.php?id=$id' class='btn btn-primary m-r-1em' name='edit'>Sửa</a>
+                    <a href='deleteMember.php?id=$id' class='btn btn-danger'>Xóa</a>
+                  </td>
+                </tr>";
+        }
+        echo "</tbody></table>";
       }
-      echo "</tbody></table>";
-    }
-  ?>
+    ?>
+  </div>
+  
 </body>
 </html>
